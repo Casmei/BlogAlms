9cd2425f2856b1b9f9dae3582dbb4e8a3482c8a5
casmei <casmei@protonmail.com>
2022-05-01 11:00:09 -0300

    problema na rota

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const { default: slugify } = require('slugify');
 const router = express.Router();
 const Category = require('../categories/Category')
+const Article = require('./Article')
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
 	res.send('ROTA DE ARTIGS');
 });
 
 router.get('/admin/articles/new', (req, res) => {
   Category.findAll().then(categories => {
-
     res.render('admin/articles/new', {categories: categories});
   })
 });
+
+router.post('/articles/save', (res,req) => {
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category
+  }).then(() => {
+    res.redirect('/admin/articles')
+  })
+})
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 const connetction = new Sequelize('blog_alms', 'root', '123456', {
 	host: 'localhost',
 	dialect: 'mysql',
-	port: 3307
+	port: 3307,
+  timezone: '-03:00'
 });
 
 module.exports = connetction;
